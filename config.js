46100e9bd6bc66b9bc81056935791c8003e7e18e
ronaldafs <R.Scholten@afsuitzendbureau.nl>
2026-09-24 15:48:28 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Supabase-koppeling voor AFS Operatie Schiphol
+const SUPABASE_URL = "https://xwspmpktdbbmxqnqzwjf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_MIvayhtNLNQn64x-3T69ZQ_-Hqwl6FG";
